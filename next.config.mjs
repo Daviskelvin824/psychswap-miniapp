e30a206e71233dbce90d63f27779df21b9610078
Daviskelvin824 <daviskelvin824@gmail.com>
2025-08-31 12:06:26 +0700

    feat: eslint

--- next.config.mjs
+++ next.config.mjs
@@ -6,6 +6,9 @@ const nextConfig = {
     config.externals.push("pino-pretty", "lokijs", "encoding");
     return config;
   },
+  eslint: {
+    ignoreDuringBuilds: true, // <- this disables ESLint checks during build
+  },
 };
 
 export default nextConfig;
